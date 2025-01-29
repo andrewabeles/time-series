2525a4a3ffa72febf13da6c723d2b6853e7f840a
andrewabeles <38055336+andrewabeles@users.noreply.github.com>
2025-01-29 16:46:42 -0500

    update

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import pandas as pd 
 import numpy as np 
 import plotly.express as px 
-from statsmodels.tsa.stattools import acf, pacf
+import plotly.graph_objects as go
+from statsmodels.tsa.stattools import adfuller, acf, pacf
 from statsmodels.tsa.seasonal import seasonal_decompose
 
 st.title("Time Series Analyzer")
@@ -13,42 +14,49 @@ if uploaded_file is not None:
     st.write("Data Preview")
     st.write(df.head())
 
-    # set datetime column as index 
     dt_colname = st.selectbox(
         "Select datetime column",
         df.select_dtypes(include=[object, 'datetime', 'datetimetz']).columns
     )
+    df[dt_colname] = pd.to_datetime(df[dt_colname], utc=True)
     df.set_index(dt_colname, inplace=True)
+    df = df.resample('D').mean()
+    st.write(df.head())
 
-    # set time series frequency 
-    freq_codes = {
-        'nanosecond': 'N',
-        'microsecond': 'U',
-        'millisecond': 'L',
-        'second': 'S',
-        'minute': 'T',
-        'hour': 'H',
-        'day': 'D',
-        'week': 'W',
-        'month start': 'MS',
-        'month end': 'ME',
-        'quarter start': 'QS',
-        'quarter end': 'QE',
-        'year start': 'YS',
-        'year end': 'YE'
-    }
-    freq_label = st.selectbox(
-        "Select frequency",
-        [k for k in freq_codes.keys()],
-        index=6
-    )
-    freq = freq_codes[freq_label]
-    df = df.asfreq(freq)
-    
     # determine target 
     y_colname = st.selectbox(
         "Select values column",
         df.select_dtypes(include='number').columns
     )
 
-    st.write(df.head())
+    # plot raw time series 
+    raw_fig = px.line(
+        df,
+        y=df[y_colname]
+    )
+    st.plotly_chart(raw_fig)
+
+    st.header("Seasonal Decomposition")
+    period = st.number_input(
+        "Period of Time Series",
+        min_value=1,
+        help="""The expected period of seasonality. For example, 4 if data is quarterly, 7 if daily, etc."""
+    )
+    decomp = seasonal_decompose(df[y_colname].dropna(), period=period)
+    decomp_df = df.copy()
+    decomp_df['trend'] = decomp.trend 
+    decomp_df['seasonal'] = decomp.seasonal 
+    decomp_df['residual'] = decomp.resid
+
+    decomp_fig = go.Figure()
+
+    # Add traces
+    decomp_fig.add_trace(go.Scatter(x=df.index, y=df[y_colname], mode='lines', name='Original'))
+    decomp_fig.add_trace(go.Scatter(x=df.index, y=decomp_df['trend'], mode='lines', name='Trend'))
+    decomp_fig.add_trace(go.Scatter(x=df.index, y=decomp_df['seasonal'], mode='lines', name='Seasonality'))
+    decomp_fig.add_trace(go.Scatter(x=df.index, y=decomp_df['residual'], mode='lines', name='Residual'))
+
+    # Update layout
+    decomp_fig.update_layout(title="Time Series Decomposition", xaxis_title="Date", yaxis_title="Value")
+
+    st.plotly_chart(decomp_fig)
